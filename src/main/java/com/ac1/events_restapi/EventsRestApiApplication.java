39bb33ec9d2206f7bc0ff5e096477552ef9cc411
RaquelKO <kuntzraquel@gmail.com>
2021-03-09 19:50:26 -0300

    criacao do projeto

---
+++ src/main/java/com/ac1/events_restapi/EventsRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.ac1.events_restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ac1/events_restapi/EventsRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ac1.events_restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
