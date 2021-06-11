cf03190904f95ef4695a86c0f9d378a59317d6fc
RaquelKO <kuntzraquel@gmail.com>
2021-06-11 15:42:12 -0300

    validações ao vender ticket

--- src/main/java/com/ac1/events_restapi/dto/TicketSellDTO.java
+++ src/main/java/com/ac1/events_restapi/dto/TicketSellDTO.java
@@ -1,9 +1,17 @@
 package com.ac1.events_restapi.dto;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
 import com.ac1.events_restapi.entities.TicketType;
 
 public class TicketSellDTO {
+
+	@NotNull(message = "This field must not be null!")
 	private TicketType type;
+
+	@NotNull(message = "This field must not be null!")
+	@PositiveOrZero(message = "This field must be a positive number!")
 	private Long idAttendee;
 
 	public TicketType getType() {
